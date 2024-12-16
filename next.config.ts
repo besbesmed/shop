0325a84b3d4f935e69bf300ebc527455f945aa63
besbesmed <besbesmed@outlook.com>
2024-12-16 19:30:08 +0100

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+module.exports = {
+  images: {
+    domains: ['readymadeui.com'], // Ajoutez les domaines nécessaires ici
+  },
+};
+export default nextConfig;
